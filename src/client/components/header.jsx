6b64a185e0277d37720b0c0efe6ec6fe8e89c44d
sasha.bashliy <sashkacrab@gmail.com>
2018-04-27 14:04:29 +0300

    done header

---
+++ src/client/components/header.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import headerStyles from "../styles/header.css";
+import hamburger from "../images/icon-mobile-menu.png";
+import searchIcon from "../images/search-icon.png";
+import profileIcon from "../images/profile-icon.png";
+
+export default class Header extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {};
+
+    this.handleHamburgerClick = this.handleHamburgerClick.bind(this);
+  }
+
+  handleHamburgerClick() {
+    //console.log(this);
+  }
+
+  render() {
+    return (
+      <header className={headerStyles.header}>
+        <a
+          className={`${headerStyles.hamburger} ${headerStyles.hamburger__link}`}
+          onClick={this.handleHamburgerClick}
+        >
+          <img
+            src={hamburger}
+            className={`${headerStyles.hamburger} ${headerStyles.hamburger__image}`}/>
+        </a>
+        <span className={headerStyles.header__brand}>
+          Brandoo
+        </span>
+        <div className={headerStyles.menuIcons}>
+          <img
+            src={searchIcon}
+            className={`${headerStyles.menuIcons__image}`}
+          />
+          <span className={headerStyles.menuIcons__divider} />
+          <img
+            src={profileIcon}
+            className={`${headerStyles.menuIcons__image}`}
+          />
+        </div>
+      </header>
+    );
+  }
+}
--- src/client/components/home.jsx
+++ src/client/components/home.jsx
@@ -1,33 +1,21 @@
-/*
- * This is a demo component the Eletrode app generator included
- * to show using Skeleton CSS lib (named base.css) and Redux
- * store for display HTML elements and managing states.
- *
- * To start your own app, please replace or remove these files:
- *
- * - this file (home.jsx)
- * - demo-buttons.jsx
- * - demo-pure-states.jsx
- * - demo-states.jsx
- * - reducers/index.jsx
- * - styles/*.css
- *
- */
 
 import React from "react";
 import "../styles/normalize.css";
 import "../styles/raleway.css";
-import skeleton from "../styles/skeleton.css";
+// import skeleton from "../styles/skeleton.css";
 import custom from "../styles/custom.css";
-import electrodePng from "../images/electrode.png";
-import DemoStates from "./demo-states";
-import DemoPureStates from "./demo-pure-states";
-import { DemoButtons } from "./demo-buttons";
+// import electrodePng from "../images/electrode.png";
+// import DemoStates from "./demo-states";
+// import DemoPureStates from "./demo-pure-states";
+// import { DemoButtons } from "./demo-buttons";
+import Header from "./header";
 /**/
 
 export default () =>
   (<div className={custom.container}>
-    {/**/}
+    <Header />
+
+    {/*
 
     <section className={custom.header}>
       <h2 className={skeleton.title}>
@@ -47,4 +35,5 @@ export default () =>
     <div className={custom["docs-section"]}>
       <DemoButtons />
     </div>
+    */}
   </div>);
